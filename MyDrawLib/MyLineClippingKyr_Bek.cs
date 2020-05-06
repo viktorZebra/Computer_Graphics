3e8ba19e54173f56470c930774cbc57cd918416d
Artem <viktorzebra54@gmail.com>
2020-05-06 12:42:41 +0300

    Добавлена реализация алгоритма отсечения Кируса-Бека для 8 лаб.работы

---
+++ MyDrawLib/MyLineClippingKyr_Bek.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace MyDrawLib
+{
+    public class MyLineClippingKyr_Bek
+    {
+        private List<Point> LinesArray;
+        private List<Point> Cutter;
+        private List<Vector> NormalVec = new List<Vector>(1);
+
+        public MyLineClippingKyr_Bek(List<Point> LinesArray, List<Point> Cutter)
+        {
+            this.LinesArray = LinesArray;
+            this.Cutter = Cutter;
+        }
+
+        public List<Point> Clipping()
+        {
+            List<Point> currLines = new List<Point>(1);
+            
+            int walkDirection = ConvexityCheck();
+
+            if (walkDirection != 0)
+            {
+                FindNormalVectors(walkDirection);
+
+                for (int j = 0; j < LinesArray.Count - 1; j += 2)
+                {
+                    float t_down = 0, t_up = 1;
+                    float t_tmp;
+
+                    Vector D = new Vector(LinesArray[j + 1], LinesArray[j]);
+                    Vector w;
+
+                    float D_sc;
+                    float W_sc;
+
+                    for (int i = 0; i < Cutter.Count; i++)
+                    {
+                        w = new Vector(LinesArray[j], Cutter[i]);
+                        D_sc = Vector.ScalarMultiplication(D, NormalVec[i]);
+                        W_sc = Vector.ScalarMultiplication(w, NormalVec[i]);
+
+                        if (D_sc == 0) // отрезок выродился в точку / D и сторона парралельны
+                        {
+                            if (W_sc < 0)
+                                break;
+                            // точка видима относительно текущей границы
+                        }
+                        else
+                        {
+                            t_tmp = -1 * (W_sc / D_sc);
+                            if (D_sc > 0) // поиск нижнего предела
+                            {
+                                if (t_tmp > 1)
+                                    break;
+                                t_down = Math.Max(t_down, t_tmp);
+                            }
+                            else // поиск верхнего предела
+                            {
+                                if (t_tmp < 0)
+                                    break;
+                                t_up = Math.Min(t_up, t_tmp);
+                            }
+                        }
+                    }
+
+                    if (t_down > t_up)
+                        break;
+
+                    currLines.Add(GetDot(t_down, LinesArray[j], LinesArray[j + 1]));
+                    currLines.Add(GetDot(t_up, LinesArray[j], LinesArray[j + 1]));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка!\nМногоугольник невыпуклый!");
+            }
+
+            return currLines;
+        }
+
+        private int GetCoordX(float t, Point start, Point end)
+        {
+            return (int)Math.Round(start.X + (end.X - start.X) * t);
+        }
+        private int GetCoordY(float t, Point start, Point end)
+        {
+            return (int)Math.Round(start.Y + (end.Y - start.Y) * t);
+        }
+        public Point GetDot(float t, Point start, Point end)
+        {
+            return new Point(GetCoordX(t, start, end), GetCoordY(t, start, end));
+        }
+
+        // Получение вершины по индексу
+        public PointF GetVertex(int index)
+        {
+            if (index < 0)
+                return Cutter[Cutter.Count + index]; // Вершина с конца
+            else
+                return Cutter[index % Cutter.Count];
+        }
+
+        // Проверка выпуклости; возвращает направление обхода
+        private int ConvexityCheck()
+        {
+            if (Cutter.Count < 3)
+                return 0;
+
+            Vector a = new Vector(Cutter[1], Cutter[0]);
+            Vector b = new Vector();
+            Vector res = new Vector();
+
+            int sign = 0;
+
+            for (int i = 0; i < Cutter.Count; i++)
+            {
+                b = new Vector(GetVertex(i + 1), GetVertex(i));
+                Vector.VectorMultiplication(a, b, ref res);
+
+                if (sign == 0)
+                    sign = Math.Sign(res.z);
+                else if ((sign != Math.Sign(res.z)) && (res.z != 0))
+                    return 0;
+
+                a = b;
+            }
+
+            if (sign == 0) // Вырождается в линию
+                return 0;
+
+            return sign;
+        }
+
+        // Нахождение векторов нормали для отсекателя
+        private void FindNormalVectors(int direction)
+        {
+            Vector b;
+            float tmp;
+            NormalVec.Clear();
+
+            for (int i = 0; i < Cutter.Count; i++)
+            {
+                b = new Vector(GetVertex(i + 1), GetVertex(i));
+
+                tmp = b.x;
+                b.x = b.y;
+                b.y = tmp;
+
+                if (direction == -1)
+                    b.y *= -1;
+                else
+                    b.x *= -1;
+
+                NormalVec.Add(b);
+            }
+        }
+    }
+}
+ 
---
+++ MyDrawLib/Vector.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace MyDrawLib
+{
+    class Vector
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector(float a = 0, float b = 0, float c = 0)
+        {
+            x = a;
+            y = b;
+            z = c;
+        }
+
+        public Vector(PointF end, PointF start)
+        {
+            x = end.X - start.X;
+            y = end.Y - start.Y;
+            z = 0;
+        }
+
+        public static void VectorMultiplication(Vector a, Vector b, ref Vector res)
+        {
+            res.x = a.y * b.z - a.z * b.y;
+            res.y = a.z * b.x - a.x * b.z;
+            res.z = a.x * b.y - a.y * b.x;
+        }
+
+        public static float ScalarMultiplication(Vector a, Vector b)
+        {
+            return a.x * b.x + a.y * b.y + a.z * b.z;
+        }
+    }
+}
